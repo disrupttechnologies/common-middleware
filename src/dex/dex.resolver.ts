59fddefef4afee8bdbfd9288fed3522fb3a3837c
shahzeb8285 <shahzeb8285@gmail.com>
2024-04-29 21:06:36 +0530

    api protected

--- src/dex/dex.resolver.ts
+++ src/dex/dex.resolver.ts
@@ -2,8 +2,12 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { DexService } from './dex.service';
 import { TokenPriceResp, TokenTradeResp } from './dto/tokenprice.input';
 import { CreateTradeDataInput } from './dto/create.trade.input';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard } from 'src/auth/gql-auth.guard';
 
+@UseGuards(GqlAuthGuard)
 @Resolver()
+
 export class DexResolver {
 
   
